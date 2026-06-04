3bf1c7c82bbb9f4580b78a7afa2a738650a4df83
abrahampcruz21 <abrahampcruz21@gmail.com>
2026-06-03 19:48:16 -0600

    feat: menú desplegable premium estilo liquid glass en registro

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './register.html',
   styleUrls: ['./register.scss'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  //VARIABLES DEL COMPONENTE
+  mostrarModal: boolean = false;
+  menuAbierto: boolean = false;
+  carreraSeleccionada: string = ''; // <-- ¡Importante declararla aquí!
+
+  //FUNCIONES DEL MENÚ DESPLEGABLE 
+  toggleDesplegable() {
+    this.menuAbierto = !this.menuAbierto;
+  }
+
+  seleccionarCarrera(carrera: string) {
+    this.carreraSeleccionada = carrera;
+    this.menuAbierto = false;
+    console.log("Carrera seleccionada:", this.carreraSeleccionada);
+  }
+
+  //LOGICÁ DE REGISTRO 
+  registrarUsuario() {
+    console.log("Usuario registrado");  
+    this.mostrarModal = true;
+  }
+}
